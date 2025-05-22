c15bbe64a4ff24c250afd9256b693c8ed1a57696
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-05-23 01:04:41 +0500

    Connect Database with signup page

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SignUp Page</title>
-    <link rel="stylesheet" href="signup-style.css">
+    <link rel="stylesheet" href="signup/signup-style.css">
 </head>
 <body>
-    <form class="form-container" method="post" action="signup-code">
+    <form class="form-container" method="post" action="signup/signup-php-code.php">
         <h1>Admission Form</h1>
 
         <label for="name">Full Name</label>
---
+++ signup/signup-php-code.php
@@ -0,0 +1,31 @@
+<?php
+    $host = "127.0.0.1";
+    $username = "root";
+    $password = "";
+    $database = "ProfileLogin";
+
+    $connection = new mysqli($host, $username, $password, $database);
+    if ($connection->connect_error) {
+        die("Connection Failed: " . $connection->connect_error);
+    }
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $password = $_POST["pass"];
+    $dateOfBirth = $_POST["dob"];
+    $gender = $_POST["gender"];
+    $department = $_POST["department"];
+    $picture = $_POST["profile_picture"];
+    $address = $_POST["address"];
+
+    $sql = "INSERT INTO Users VALUES(NULL, '$name', '$password', '$email', '$dateOfBirth', '$gender', '$department', '$picture', '$address')";
+
+    // echo $sql;
+     $db_response = $connection->query($sql);
+
+    if ($db_response === true) {
+        echo "<script>alert('Record Inserted');</script>";
+    } else {
+        echo "<script>alert('Record not Inserted');</script>";
+    }
+?>
